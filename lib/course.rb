84343353796497ad143669e2c6d9cb66092cd77a
Romario Gustave <romagustave10@gmail.com>
2020-01-14 21:41:05 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,5 +1,17 @@
 class Course 
   attr_accessor :title, :schedule, :description 
-
+  @@all = []
+  
+  def initialize 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.reset.all 
+    self.clear 
+  end 
 end 
 
